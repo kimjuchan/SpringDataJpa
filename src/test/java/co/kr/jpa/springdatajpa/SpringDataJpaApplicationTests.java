9ceed97f99277948249057fb9b3f600012d26b76
chanchan <42960960+kimjuchan@users.noreply.github.com>
2024-02-17 17:52:19 +0900

    init commit

---
+++ src/test/java/co/kr/jpa/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.jpa.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
