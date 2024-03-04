ad34790ff24cba0b467c93f52710658c29a369c7
kjuchan <kjuchan92@naver.com>
2024-03-04 14:16:59 +0900

    #jasypt db 정보 암호화 설정 작업

--- src/main/java/co/kr/jpa/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/co/kr/jpa/springdatajpa/SpringDataJpaApplication.java
@@ -1,9 +1,11 @@
 package co.kr.jpa.springdatajpa;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/co/kr/jpa/springdatajpa/config/JasyptConfigTest.java
@@ -0,0 +1,42 @@
+package co.kr.jpa.springdatajpa.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JasyptConfigTest {
+
+    @Test
+    public void jasyptEncodingTest(){
+        String userName_before = "juchan";
+        String userName_after = "";
+
+
+        String url = "jdbc:mariadb://localhost:3306/springdata";
+        String userName = "root";
+        String paswword = "1q2w3e4r!@#";
+
+
+        String key = "juchan123!@#";
+
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        pbeEnc.setPassword(key);
+        pbeEnc.setIvGenerator(new RandomIvGenerator());
+
+        url = pbeEnc.encrypt(url);
+        userName =pbeEnc.encrypt(userName);
+        paswword = pbeEnc.encrypt(paswword);
+
+        userName_after = pbeEnc.encrypt(userName_before);
+        System.out.println("###encrypt value :: >> " + userName_after);
+        System.out.println("###decrypt  value :: >> " + pbeEnc.decrypt(userName_after));
+        System.out.println("###decrypt URL value ::  " + pbeEnc.decrypt(url) + " && encrypt value :: " + url);
+        System.out.println("###decrypt USER_NAME value ::  " + pbeEnc.decrypt(userName) + " && encrypt value :: " + userName);
+        System.out.println("###decrypt PWD value ::  " + pbeEnc.decrypt(paswword) + " && encrypt value :: " + paswword);
+
+    }
+
+}
